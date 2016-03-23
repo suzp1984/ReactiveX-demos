0d1ed582ca7ca8ee1fac78fcecf373afb8104544
suzp1984 <suzp1984@gmail.com>
2016-03-23 17:20:40 +0800

    init repo

---
+++ Rxjava/rxjava-demos/src/main/java/io/github/suzp1984/rx/demos/App.java
@@ -0,0 +1,13 @@
+package io.github.suzp1984.rx.demos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
