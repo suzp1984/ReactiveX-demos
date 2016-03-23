02748cbf9f7c7029dedd150022b5d25ebffbf1ac
suzp1984 <suzp1984@gmail.com>
2016-03-23 18:03:23 +0800

    add a rxjava sample

--- Rxjava/rxjava-demos/src/main/java/io/github/suzp1984/rx/demos/App.java
+++ Rxjava/rxjava-demos/src/main/java/io/github/suzp1984/rx/demos/App.java
@@ -1,5 +1,8 @@
 package io.github.suzp1984.rx.demos;
 
+import rx.Observable;
+import rx.Observer;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "--- Rxjava samples ---" );
+        Integer[] intArray = new Integer[] {1, 2, 3, 4, 5};
+        Observable.from(intArray).subscribe(new Observer<Integer>() {
+            public void onCompleted() {
+                System.out.println("completed.");
+            }
+
+            public void onError(Throwable throwable) {
+                System.out.println(throwable.toString());
+            }
+
+            public void onNext(Integer integer) {
+                System.out.println(integer);
+            }
+        });
     }
 }
