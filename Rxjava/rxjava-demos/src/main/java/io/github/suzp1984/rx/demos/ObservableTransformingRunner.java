39b028df9b56d5312e4c255f6d37c46771c40ba8
suzp1984 <suzp1984@gmail.com>
2016-03-25 23:46:20 +0800

    add observable flatmap and concatmap samples

--- Rxjava/rxjava-demos/src/main/java/io/github/suzp1984/rx/demos/ObservableTransformingRunner.java
+++ Rxjava/rxjava-demos/src/main/java/io/github/suzp1984/rx/demos/ObservableTransformingRunner.java
@@ -12,5 +12,24 @@ public class ObservableTransformingRunner implements IRxRunner {
         Observable.range(1, 10).buffer(2).subscribe(x -> System.out.println(x),
                 x -> System.out.println(x.toString()),
                 () -> System.out.println("completed"));
+
+        System.out.println("--- Observable flatmap ---");
+        Observable.range(1, 10).flatMap(x -> {
+                    return Observable.just(x * x);
+                }
+        ).subscribe(x -> System.out.println(x),
+                x -> System.out.println(x.toString()),
+                () -> System.out.println("completed"));
+
+        System.out.println("--- Observable concatmap ---");
+        Observable.range(1, 10).concatMap(x -> {
+            return Observable.just(x * x);
+        }).subscribe(x -> System.out.println(x),
+                x -> System.out.println(x.toString()),
+                () -> System.out.println("completed"));
+
+
+        System.out.println("--- Observable groupby ---");
+        // Observable.range(1, 10).group
     }
 }
